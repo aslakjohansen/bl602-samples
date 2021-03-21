b9f0e20a1621a0470d2f9ab2fb862e0b3731f942
Aslak Johansen <aslakj@gmail.com>
2021-03-21 16:50:21 +0100

    Cleanup

--- topics/adc/adc/main.c
+++ topics/adc/adc/main.c
@@ -59,27 +59,28 @@ void TaskSample (void *pvParameters)
         float coe=1.0;
         unsigned int val = (unsigned int)(((regval&0xffff)>>4)/coe);
         
-        printf("regval: %u %u\n", regval, val);
+/*        printf("regval: %u %u\n", regval, val);*/
+        printf("%u\n", val);
     }
 }
 
-void TaskBlink (void *pvParameters)
-{
-    bl_gpio_enable_output(PIN, 0, 0);
-    while (1) {
-        bl_gpio_output_set(PIN, 1);
-        vTaskDelay(DELAY);
-        bl_gpio_output_set(PIN, 0);
-        vTaskDelay(DELAY);
-    }
-}
+/*void TaskBlink (void *pvParameters)*/
+/*{*/
+/*    bl_gpio_enable_output(PIN, 0, 0);*/
+/*    while (1) {*/
+/*        bl_gpio_output_set(PIN, 1);*/
+/*        vTaskDelay(DELAY);*/
+/*        bl_gpio_output_set(PIN, 0);*/
+/*        vTaskDelay(DELAY);*/
+/*    }*/
+/*}*/
 
 void bfl_main(void)
 {
     system_init();
     bl_uart_init(0, 16, 7, 255, 255, 9600);
     printf("booted\n");
-    xTaskCreate(TaskBlink, "Blink", 1024, NULL, 1, NULL);
+/*    xTaskCreate(TaskBlink, "Blink", 1024, NULL, 1, NULL);*/
     xTaskCreate(TaskSample, "Blink", 1024, NULL, 1, NULL);
     vTaskStartScheduler();
 }
